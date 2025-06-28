c73d44abe12a8353ad858e640c5405d4ba7c95d2
Adam Lisicki <adam.lisicki@adamlisicki.pl>
2025-06-28 12:43:42 +0000

    configure tf cloud

--- oci/provider.tf
+++ oci/provider.tf
@@ -1,13 +1,26 @@
-
 terraform {
   required_providers {
     oci = {
-      source = "oracle/oci"
+      source  = "oracle/oci"
       version = "7.7.0"
+    }
+  }
+
+  backend "remote" {
+    organization = "OCI-infra"
+    hostname     = "app.terraform.io"
+
+    workspaces {
+      name = "terraform"
     }
   }
 }
 
+
 provider "oci" {
-  region = "eu-frankfurt-1"
+  tenancy_ocid = var.tenancy_ocid
+  user_ocid    = var.user_ocid
+  fingerprint  = var.fingerprint
+  private_key = replace(var.private_key, "\\n", "\n")
+  region       = var.region
 }
--- oci/variables.tf
+++ oci/variables.tf
@@ -1,3 +1,10 @@
 variable "compartment_ocid" {
   default = "ocid1.tenancy.oc1..aaaaaaaaor3rztm3yommn57nkd6epscnzk5s4cqcyc72papjmchde56cysma"
 }
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key" {}
+variable "region" {
+  default = "eu-frankfurt-1"
+}
